d3ca7443e5fe333f5187fe07489125b1c282a481
José Alberto García Torres <josealberto629@gmail.com>
2015-09-16 13:34:44 -0500

    Back

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+// Simple custom jQuery functions for plugins
+// Made by Alejandro Martínez http://github.com/AlejandroMAL
+
+$(document).ready(function() {
+	$(".lightGallery").lightGallery({
+	    	thumbnail : false,
+	    	controls: false
+	}); 
+	$('.flexslider').flexslider({
+			animation: "slide"
+	});
+});
